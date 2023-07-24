f3f2180785c6c01da107b5b2cefc0d49db29373e
JabalThoriq16 <jabal.thoriq16@mhs.uinjkt.ac.id>
2023-07-24 20:27:43 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Portofolio
+
+
+Pengembangan Portofolio yang terintegrasi dengan repository github.
+
+## Daftar Isi
+
+- [Demo](#demo)
+- [Tentang Proyek](#tentang-proyek)
+- [Teknologi yang Digunakan](#teknologi-yang-digunakan)
+- [Cara Menggunakan](#cara-menggunakan)
+- [Cara Berkontribusi](#cara-berkontribusi)
+- [Lisensi](#lisensi)
+
+## Demo
+
+Tampilkan tautan atau gambar demo proyek Anda di sini (jika ada).
+
+## Tentang Proyek
+
+Proyek ini dikembangkan untuk pembuatan portofolio yang lansung terintegrasi dengan github.
+
+## Teknologi yang Digunakan
+
+Daftar teknologi utama yang Anda gunakan untuk membangun proyek. Misalnya:
+
+- Javascript
+- Node.js
+- React js
+
+## Cara Menggunakan
+
+Tuliskan langkah-langkah yang jelas tentang cara menggunakan proyek Anda. Ini harus mencakup instruksi instalasi, konfigurasi, dan cara menjalankan aplikasi (jika proyek ini adalah aplikasi).
+
+1. Langkah pertama
+   npm install
+2. Langkah kedua
+   npm run start
+
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,58 @@
+import React, {useState,useEffect} from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+// import leaf from "../../Assets/Projects/leaf.png";
+// import emotion from "../../Assets/Projects/emotion.png";
+// import editor from "../../Assets/Projects/codeEditor.png";
+// import chatify from "../../Assets/Projects/chatify.png";
+// import suicide from "../../Assets/Projects/suicide.png";
+// import bitsOfCode from "../../Assets/Projects/blog.png";
+
+function Projects() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await fetch(
+          "https://api.github.com/users/jabalthoriq00/repos"
+        );
+        const data = await response.json();
+        setProjects(data);
+      } catch (error) {
+        console.error("Error fetching data from GitHub API:", error);
+      }
+    };
+    fetchProjects();
+  }, []);
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          {projects.map((project) => (
+            <Col md={4} className="project-card" key={project.id}>
+              <ProjectCard
+                imgPath={project.imgPath}
+                isBlog={false}
+                title={project.name}
+                description={project.description}
+                ghLink={project.html_url}
+                demoLink={project.homepage}
+              />
+            </Col>
+          ))}
+        </Row>{" "}
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
